896fcf30f15174858c720da8812a05350abdb187
John Maged <glaxek@gmail.com>
2022-02-04 02:08:17 +0200

    Main script now print final results

--- main.py
+++ main.py
@@ -4,15 +4,34 @@ from pcparts import settings as pcparts_settings
 
 from pcparts.spiders.EgPrices import EgpricesSpider
 from pcparts.spiders.CompuArt import CompuartSpider
+from pcparts.spiders.ElbadrGroup import ElBadrGroupSpider
+from pcparts.spiders.MaximumHardware import MaximumHardwareSpider
 
 
 def main():
+    spiders = [
+        EgpricesSpider,
+        CompuartSpider,
+        ElBadrGroupSpider,
+        MaximumHardwareSpider,
+    ]
+    crawlers = {}
     settings = Settings()
     settings.setmodule(pcparts_settings)
     process = CrawlerProcess(settings=settings)
-    process.crawl(EgpricesSpider)
-    process.crawl(CompuartSpider)
+    for spider in spiders:
+        crawlers[spider.name] = process.create_crawler(spider)
+    for crawler in crawlers.values():
+        process.crawl(crawler)
     process.start()
+
+    print("---- Finished ----")
+    for name, crawler in crawlers.items():
+        stats = crawler.stats.get_stats()
+        print(f"{name = }")
+        print(f"log_count/ERROR: {stats.get('log_count/ERROR', 0)}")
+        print(f"item_scraped_count: {stats.get('item_scraped_count', 0)}")
+        print("-----")
 
 
 if __name__ == "__main__":
