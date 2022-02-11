5755cdd87019817b1aadbd44cd3146b993d5d67e
John Maged <ijohnmaged@icloud.com>
2022-02-11 18:28:16 +0200

    added user agent to settings

--- pcparts/settings.py
+++ pcparts/settings.py
@@ -32,7 +32,7 @@ NEWSPIDER_MODULE = "pcparts.spiders"
 FEED_EXPORT_ENCODING = "utf-8"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = 'pcparts (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
