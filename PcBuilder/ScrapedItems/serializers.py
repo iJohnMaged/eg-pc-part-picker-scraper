a6505be738f3a8532d11e25d7d5ff6aef1a8bdf9
John Maged <ijohnmaged@icloud.com>
2022-02-14 22:06:02 +0200

    Changed data format

--- PcBuilder/ScrapedItems/serializers.py
+++ PcBuilder/ScrapedItems/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.db import models
 from .models import *
 
 
@@ -14,6 +15,19 @@ class StoreSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class ComponentListSerializer(serializers.ListSerializer):
+    def to_representation(self, data):
+        iterable = data.all() if isinstance(data, models.Manager) else data
+        stores_map = {}
+        for item in iterable:
+            rep = self.child.to_representation(item)
+            if rep["store"]["name"] in stores_map:
+                stores_map[rep["store"]["name"]].append(rep)
+            else:
+                stores_map[rep["store"]["name"]] = [rep]
+        return [{"label": key, "options": value} for key, value in stores_map.items()]
+
+
 class ComponentSerializer(serializers.ModelSerializer):
     category = CateogrySerializer(read_only=True)
     store = StoreSerializer(read_only=True)
@@ -21,6 +35,7 @@ class ComponentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Component
         fields = "__all__"
+        list_serializer_class = ComponentListSerializer
 
 
 class BuildSerializer(serializers.ModelSerializer):
